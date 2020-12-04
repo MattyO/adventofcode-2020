bbbb2297acff8750f642f4222d27ce2b0040d5dd
Matt <odonnell004@gmail.com>
2020-12-04 02:27:50 -0500

    four

---
+++ four/main.py
@@ -0,0 +1,83 @@
+import collections
+import re
+
+Field = collections.namedtuple('Field', ['name', 'value'])
+def read_passports(filename):
+    passports = []
+    with open(filename) as f:
+        lines = f.readlines()
+
+    passport_lines = []
+    for line in lines:
+        if line.strip() == "":
+            passports.append(Passport(passport_lines))
+            passport_lines.clear()
+        else:
+            passport_lines.append(line)
+    if len(passport_lines) > 0:
+        passports.append(Passport(passport_lines))
+
+    return passports
+def height_validation(f):
+    result = re.search("(?P<num>[0-9]+)(?P<units>in|cm)", f)
+
+    if result is None:
+        return False
+
+    elif result.group('units') == 'in':
+        return 59 <= int(result.group('num')) <=76
+    elif result.group('units') == 'cm':
+        return 150 <= int(result.group('num')) <= 193
+
+    return False
+
+
+
+
+class Passport():
+    valid_fields = sorted(['byr','iyr','eyr', 'hgt', 'hcl', 'ecl', 'pid'])
+    validations = {
+        'byr': [
+            lambda f: bool(re.search("^[0-9]{4}$", f)),
+            lambda f: 1920 <= int(f) <= 2002
+        ],
+        'iyr': [
+            lambda f: bool(re.search("^[0-9]{4}$", f)),
+            lambda f: 2010 <= int(f) <= 2020
+        ],
+        'eyr': [
+            lambda f: bool(re.search("^[0-9]{4}$", f)),
+            lambda f: 2020 <= int(f) <= 2030
+        ],
+        'hgt': [
+            height_validation
+         ],
+        'hcl': [
+            lambda f: bool(re.search("^#[0-9a-f]{6}$", f)),
+        ],
+        'ecl': [
+            lambda f: f in ['amb', 'blu', 'brn', 'gry', 'grn','hzl', 'oth'],
+        ],
+        'pid': [
+            lambda f: bool(re.search("^[0-9]{9}$", f)),
+        ],
+    }
+
+    def __init__(self, lines):
+        self.fields = []
+        for line in lines:
+            for item in line.split(" "):
+                k, v = item.split(":")
+                self.fields.append(Field(k,v.strip()))
+
+    def is_valid(self):
+        passport_fields = [ field.name for field in self.fields ]
+        return all(valid_field in passport_fields and self.is_field_valid(valid_field) for valid_field in self.valid_fields)
+
+    def get_field(self, code):
+        return next(filter(lambda f: f.name == code, self.fields), None)
+
+    def is_field_valid(self, code):
+        f = self.get_field(code)
+        vs = [v(f.value) for v in self.validations[f.name]]
+        return all(v(f.value) for v in self.validations[f.name])
---
+++ tests/four/test_main.py
@@ -0,0 +1,116 @@
+import unittest
+import four.main
+
+class PassportTest(unittest.TestCase):
+    def test_passport(self):
+        self.assertEqual(len(four.main.read_passports("four/data_example.txt")),4) 
+
+    def test_passport_create(self):
+        lines = ['ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
+                 'byr:1937 iyr:2017 cid:147 hgt:183cm']
+
+        p = four.main.Passport(lines)
+        self.assertIsInstance(p, four.main.Passport)
+
+    def test_passport_fields(self):
+        lines = ['ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
+                 'byr:1937 iyr:2017 cid:147 hgt:183cm']
+
+        p = four.main.Passport(lines)
+        self.assertEqual(len(p.fields), 8)
+
+    def test_passport_is_valid(self):
+        lines = ['ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
+                 'byr:1937 iyr:2017 cid:147 hgt:183cm']
+
+        p = four.main.Passport(lines)
+        self.assertTrue(p.is_valid())
+
+    def test_passport_is_not_valid(self):
+        lines = ['iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884',
+                 'hcl:#cfa07d byr:1929',]
+
+        p = four.main.Passport(lines)
+        self.assertFalse(p.is_valid())
+
+    def test_num_of_example_valid_passports(self):
+        passports = four.main.read_passports("four/data_example.txt")
+        self.assertEqual(len(list(filter(lambda p: p.is_valid(), passports))), 2)
+
+    def test_num_of_example_valid_passports_puzzle(self):
+        passports = four.main.read_passports("four/data_puzzle.txt")
+        self.assertEqual(len(list(filter(lambda p: p.is_valid(), passports))), 160)
+
+    def test_get_field(self):
+        lines = ['iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884',
+                 'hcl:#cfa07d byr:1929',]
+
+        p = four.main.Passport(lines)
+        field = p.get_field('iyr')
+        self.assertEqual(field.name, 'iyr')
+        self.assertEqual(field.value, '2013')
+
+    def test_bry_is_valid(self):
+        self.assertTrue(four.main.Passport(['byr:2002']).is_field_valid('byr'))
+
+    def test_bry_is_valid(self):
+        self.assertFalse(four.main.Passport(['byr:2003']).is_field_valid('byr'))
+
+    def test_iyr_is_valid(self):
+        self.assertTrue(four.main.Passport(['iyr:2010']).is_field_valid('iyr'))
+
+    def test_iyr_is_not_valid(self):
+        self.assertFalse(four.main.Passport(['iyr:2009']).is_field_valid('iyr'))
+
+    def test_eyr_is_valid(self):
+        self.assertTrue(four.main.Passport(['eyr:2030']).is_field_valid('eyr'))
+
+    def test_eyr_is_not_valid(self):
+        self.assertFalse(four.main.Passport(['eyr:2031']).is_field_valid('eyr'))
+
+    def test_hcl_is_valid(self):
+        self.assertTrue(four.main.Passport(['hcl:#123abc']).is_field_valid('hcl'))
+
+    def test_hcl_is_not_valid(self):
+        self.assertFalse(four.main.Passport(['hcl:#123abz']).is_field_valid('hcl'))
+
+    def test_hcl_is_not_valid2(self):
+        self.assertFalse(four.main.Passport(['hcl:123abc']).is_field_valid('hcl'))
+
+    def test_ecl_is_valid(self):
+        self.assertTrue(four.main.Passport(['ecl:brn']).is_field_valid('ecl'))
+
+    def test_ecl_is_valid2(self):
+        self.assertTrue(four.main.Passport(['ecl:hzl']).is_field_valid('ecl'))
+
+    def test_ecl_is_not_valid(self):
+        self.assertFalse(four.main.Passport(['ecl:wat']).is_field_valid('ecl'))
+
+    def test_pid_is_valid(self):
+        self.assertTrue(four.main.Passport(['pid:000000001']).is_field_valid('pid'))
+
+    def test_pid_is_not_valid(self):
+        self.assertFalse(four.main.Passport(['pid:0123456789']).is_field_valid('pid'))
+
+    def test_hgt_is_valid(self):
+        self.assertTrue(four.main.Passport(['hgt:60in']).is_field_valid('hgt'))
+
+    def test_hgt_is_valid(self):
+        self.assertTrue(four.main.Passport(['hgt:190cm']).is_field_valid('hgt'))
+
+    def test_hgt_is_not_valid(self):
+        self.assertFalse(four.main.Passport(['hgt:190in']).is_field_valid('hgt'))
+
+    def test_hgt_is_not_valid(self):
+        self.assertFalse(four.main.Passport(['hgt:190']).is_field_valid('hgt'))
+
+    def test_valid_passports(self):
+        passports = four.main.read_passports("four/data_valid.txt")
+
+        self.assertEqual(len(list(filter(lambda p: p.is_valid(), passports))), 4)
+
+    def test_invalid_passports(self):
+        passports = four.main.read_passports("four/data_invalid.txt")
+
+        self.assertEqual(len(list(filter(lambda p: not p.is_valid(), passports))), 4)
+
