b11816bd3fc391d6a656038868688f49c8440da4
Matt <odonnell004@gmail.com>
2020-12-08 13:47:43 -0500

    use new lib in refactoring parse

--- six/main.py
+++ six/main.py
@@ -1,21 +1,8 @@
 import functools
+import lib.parser
 
 def parse(filename):
-    questions= []
-    with open(filename) as f:
-        lines = f.readlines()
-
-    question_lines = []
-    for line in lines:
-        if line.strip() == "":
-            questions.append(question_lines)
-            question_lines = []
-        else:
-            question_lines.append(line.strip())
-    if len(question_lines) > 0:
-        questions.append(question_lines)
-
-    return questions
+    return lib.parser.file_grouped_by_blanke_lines(filename)
 
 def question_count(lines):
     return len(functools.reduce(lambda line_one, line_two: set(line_one) | set(line_two), lines ))
