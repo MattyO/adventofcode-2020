726d76bcfba9f5bf71ac93a78f9495bfed7f2c63
Matt <odonnell004@gmail.com>
2020-12-09 00:45:20 -0500

    cleanup for slight readablity improvement

--- nine/main.py
+++ nine/main.py
@@ -3,7 +3,6 @@ import itertools
 def is_valid(num, num_range):
     return any([combo for combo in itertools.combinations(num_range, 2) if sum(combo) == num])
 
-
 def is_valid_window(item_list, window_size):
     for i, item in enumerate(item_list[window_size:]):
         if not is_valid(item, item_list[i:i+window_size]):
@@ -13,11 +12,13 @@ def is_valid_window(item_list, window_size):
 def contiguous_set(item_list, num):
     for i, item in enumerate(item_list):
         for counter in range(i, len(item_list)):
-            sum_list = item_list[i:counter]
-            if sum(sum_list) > num:
+            list_to_sum = item_list[i:counter]
+            the_sum = sum(item_list[i:counter])
+
+            if the_sum > num:
                 continue
-            elif sum(sum_list) == num:
-                return sum_list
+            elif the_sum == num:
+                return list_to_sum
 
     return None
 
