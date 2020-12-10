f7180f86adea3f6b67fffc0b9ef2f6c93ff1fead
Matt <odonnell004@gmail.com>
2020-12-10 02:06:16 -0500

    remove print

--- ten/main.py
+++ ten/main.py
@@ -12,7 +12,6 @@ def pairwise(iterable):
 def voltage_difference(adapter_list):
 
     adapter_list = sorted([0] + list(adapter_list))
-    print(collections.Counter(list(map(lambda t: t[1] - t[0], pairwise(adapter_list))) + [3]))
     return sum(map(lambda t: t[1] - t[0], pairwise(adapter_list))) + 3
 
 def arangements(adapter_list):
@@ -23,6 +22,5 @@ def arangements(adapter_list):
     groups_of_one  = functools.reduce(lambda x,y: str(x)+str(y),  [str(d) for d in differences]).split("3")
     max_group_length = max([len(g) for g in groups_of_one])
     perms = {**{0: 0, 1:0}, **{2: 2, 3: 4, 4: 7}}
-            
     return functools.reduce(lambda x, y: x * y, filter(lambda i: i > 0, map(lambda g: perms[len(g)], groups_of_one)))
 
