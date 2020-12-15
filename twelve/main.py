faa6c79e605c4706eaf4836cd4f5a3746ac47541
Matt <odonnell004@gmail.com>
2020-12-15 12:18:15 -0500

    outside in twelve

---
+++ twelve/main.py
@@ -0,0 +1,59 @@
+from lib.computer import InstructionSet, instruction
+
+class InstructionSet(lib.computer.InstructionSet):
+
+    @instruction('N')
+    def north(i, world):
+        pass
+
+    @instruction('S')
+    def south(i, world):
+        pass
+
+    @instruction('E')
+    def east(i, world):
+        pass
+
+    def west(i, world):
+        pass
+    @instruction('L')
+    def left(i, world):
+        pass
+
+    @instruction('R')
+    def right(i, world):
+        pass
+
+    @instruction('F')
+    def forward(i, world):
+        pass
+
+def ferry_parser(line):
+    pass
+
+FerryComputer = lib.computer.computer(
+    instructions = [
+        north,
+        south,
+        east,
+        west,
+        left,
+        right,
+        forward
+    ],
+    world = {
+        'direction': 'N', 
+        'possition': (0,0),
+    },
+    parser = ferry_parser
+    )
+
+def find_location(filename):
+    final_locaiton = None
+    with open(filename) as f:
+        fc = FerryComputer(f.readlines())
+        fc.run()
+        final_locaiton = fc.world['position']
+
+    return final_locaiton
+
